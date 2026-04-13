1b028f0e658e9eb586ae64eaa101cbe3e4bde5ea
Kenji <kenji.yoshida.home.2026@gmail.com>
2026-04-13 10:04:26 +0900

    dissolve

--- src/pbf-extension.js
+++ src/pbf-extension.js
@@ -1,34 +1,32 @@
 import { PBF } from "./pbf-base.js";
 import * as spatial from "./extension/spatial.js";
 import * as manipulate from "./extension/manipulate.js";
-import { nearPoint } from "./extension/nearPoint.js"; // 独立したファイルをインポート
-import { contain } from "./extension/contain.js";     // 独立したファイルをインポート
+import { nearPoint } from "./extension/nearPoint.js";
+import { contain } from "./extension/contain.js";
+import { dissolve } from "./extension/dissolve.js";
 
 const setGetter = (name, func) => { Object.defineProperty(PBF.prototype, name, { get: func, configurable: false, enumerable: false }); };
 const setPrototype = (name, func) => { Object.defineProperty(PBF.prototype, name, { value: func, configurable: false, enumerable: false }); };
 
-// 静的メソッド
 Object.defineProperty(PBF, 'update', { value: manipulate.update, configurable: false, enumerable: false });
 Object.defineProperty(PBF, 'concatinate', { value: manipulate.concatinate, configurable: false, enumerable: false });
 
-// Getters
 setGetter("count", function () { return manipulate.count(this); });
 setGetter("lint", function () { return manipulate.lint(this); });
 
-// 空間演算
 setPrototype("centroid", function (i) { return spatial.centroid(this, i); });
 setPrototype("area", function (i) { return spatial.area(this, i); });
-setPrototype("contain", function (pt, one) { return contain(this, pt, one); }); // 変更
-setPrototype("nearPoint", function (pt, count, dist) { return nearPoint(this, pt, count, dist); }); // 変更
+setPrototype("contain", function (pt, one) { return contain(this, pt, one); });
+setPrototype("nearPoint", function (pt, count, dist) { return nearPoint(this, pt, count, dist); });
 
-// データ操作
 setPrototype("clone", function (opt) { return manipulate.clone(this, opt); });
 setPrototype("rename", function (name) { return manipulate.clone(this, { name }); });
 setPrototype("filter", function (f) { return manipulate.clone(this, { filter: f }); });
 setPrototype("map", function (m) { return manipulate.clone(this, { map: m }); });
 setPrototype("classify", function (k) { return manipulate.classify(this, k); });
-setPrototype("mergePolygonByProperty", function (p) { return manipulate.mergePolygonByProperty(this, p); });
 setPrototype("header", function (meta) { return manipulate.header(this, meta); });
 setPrototype("concat", function (...args) { return manipulate.concatinate([this, ...args], this.name()); });
+
+setPrototype("dissolve", function (p) { return dissolve(this, p); });
 
 export { PBF };
