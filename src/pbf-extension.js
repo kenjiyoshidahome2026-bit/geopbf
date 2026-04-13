29bfcbd76e14fba263fc80247571871e9fdca797
Kenji <kenji.yoshida.home.2026@gmail.com>
2026-04-13 12:06:24 +0900

    view

--- src/pbf-extension.js
+++ src/pbf-extension.js
@@ -6,6 +6,7 @@ import { contain } from "./extension/contain.js";
 import { dissolve } from "./extension/dissolve.js";
 import { analyzeTopology, neighbors, mesh, merge } from "./extension/topology.js"; // 変更
 import { toTopoJSON } from "./extension/topojson.js";
+import { drawGeometry, view } from "./extension/view.js";
 
 const setGetter = (name, func) => { Object.defineProperty(PBF.prototype, name, { get: func, configurable: false, enumerable: false }); };
 const setPrototype = (name, func) => { Object.defineProperty(PBF.prototype, name, { value: func, configurable: false, enumerable: false }); };
@@ -34,5 +35,9 @@ setPrototype("neighbors", function (id) { return neighbors(this, id); });
 setPrototype("mesh", function (f) { return mesh(this, f); });   // 追加
 setPrototype("merge", function (f) { return merge(this, f); }); // 追加
 setGetter("topojson", function () { return toTopoJSON(this); });
+
+setPrototype("drawGeometry", function (n) { return drawGeometry(this, n); });
+setPrototype("context", function (ctx, proj) { this.ctx = ctx; this.proj = proj; return this; });
+setPrototype("view", function (canvas, props) { return view(this, canvas, props); });
 
 export { PBF };
